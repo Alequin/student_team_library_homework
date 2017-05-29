a1a8cc6f1d3e354c8a51224efd6fdde79d417195
James Cox <j-cox03@hotmail.com>
2017-05-29 15:49:16 +0100

    Made student class with attr_accessor for variables and talk method.

--- student/spec/student_spec.rb
+++ student/spec/student_spec.rb
@@ -1,7 +1,33 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("student.rb")
+require_relative("../student.rb")
 
 class TestStudent < Minitest::Test
+
+  def setup
+    @student = Student.new("James", 13)
+  end
+
+  def test_get_name
+    assert_equal("James", @student.name)
+  end
+
+  def test_set_name
+    @student.name = "Andrew"
+    assert_equal("Andrew", @student.name)
+  end
+
+  def test_get_cohort
+    assert_equal(13, @student.cohort)
+  end
+
+  def test_set_cohort
+    @student.cohort = 14
+    assert_equal(14, @student.cohort)
+  end
+
+  def test_student_talk
+    assert_equal("I can talk", @student.talk)
+  end
 
 end
